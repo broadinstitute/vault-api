9f2397b5bc117bfe4fb80b45337ec411fe004f6c
Joel Thibault <thibault@broadinstitute.org>
2015-02-17 12:27:36 -0500

    Initial Hello World commit

---
+++ build.sbt
@@ -0,0 +1,4 @@
+name := "Vault Orchestration"
+
+scalaVersion := "2.11.1"
+
---
+++ src/main/scala/org/broadinstitute/dsde/vault/HelloWorld.scala
@@ -0,0 +1,5 @@
+package org.broadinstitute.dsde.vault
+
+object HelloWorld {
+	def main(args: Array[String]) = println("Hello, World.  This is the Vault Orchestration Service.")
+}
