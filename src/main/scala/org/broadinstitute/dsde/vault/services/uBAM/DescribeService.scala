0beff71af9c00ae8aa964f4d98a878e95dc77f57
Gregory Rushton <grushton@broadinstitute.org>
2015-03-06 10:33:45 -0500

    First pass at a pass-through implementation of the describe ubam service. (cherry picked from commit 96867ac)

--- src/main/scala/org/broadinstitute/dsde/vault/services/uBAM/DescribeService.scala
+++ src/main/scala/org/broadinstitute/dsde/vault/services/uBAM/DescribeService.scala
@@ -1,16 +1,16 @@
 package org.broadinstitute.dsde.vault.services.uBAM
 
+import akka.actor.Props
 import com.wordnik.swagger.annotations._
-import spray.http.MediaTypes._
+import org.broadinstitute.dsde.vault.DmClientService
+import org.broadinstitute.dsde.vault.model._
 import spray.json._
 import spray.routing._
-import org.broadinstitute.dsde.vault.model._
 
 object DescribeJsonProtocol extends DefaultJsonProtocol {
   implicit val metadata = jsonFormat12(Metadata)
   implicit val json = jsonFormat3(uBAM)
 }
-import DescribeJsonProtocol._
 
 @Api(value = "/ubams", description = "uBAM Service", produces = "application/json", position = 0)
 trait DescribeService extends HttpService {
@@ -21,8 +21,8 @@ trait DescribeService extends HttpService {
     nickname = "ubam_describe",
     httpMethod = "GET",
     produces = "application/json",
-    response=classOf[uBAM],
-    notes="Supports arbitrary metadata keys, but this is not represented well in Swagger (see the 'additionalMetadata' note below)"
+    response = classOf[uBAM],
+    notes = "Supports arbitrary metadata keys, but this is not represented well in Swagger (see the 'additionalMetadata' note below)"
   )
   @ApiImplicitParams(Array(
     new ApiImplicitParam(name = "id", required = true, dataType = "string", paramType = "path", value = "uBAM Vault ID")
@@ -32,35 +32,16 @@ trait DescribeService extends HttpService {
     new ApiResponse(code = 404, message = "Vault ID Not Found"),
     new ApiResponse(code = 500, message = "Vault Internal Error")
   ))
-  def describeRoute =
+  def describeRoute = {
     path("ubams" / Segment) {
       id =>
-        get {
-          respondWithMediaType(`application/json`) {
-            complete {
-              uBAM(
-                id,
-                Map("bam"->"sample BOSS id 1", "bai"->"sample BOSS id 2", "..."->"more files"),
-                Metadata(
-                  "dummy ownerId",
-                  "dummy md5",
-                  "dummy project",
-                  "dummy individualAlias",
-                  "dummy sampleAlias",
-                  "dummy readGroupAlias",
-                  "dummy libraryName",
-                  "dummy sequencingCenter",
-                  "dummy platform",
-                  "dummy platformUnit",
-                  "dummy runDate",
-                  "..."
-                )
-              ).toJson.prettyPrint
-            }
-          }
+        requestContext => {
+          val dmService = actorRefFactory.actorOf(Props(new DmClientService(requestContext)))
+          dmService ! DmClientService.QueryUBam(id)
         }
-      }
-  
+    }
+  }
+
 }
 
 
