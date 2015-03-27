40cf88c4da8fd0cdd9123275e057b997111c5b3b
Gregory Rushton <grushton@broadinstitute.org>
2015-03-27 15:37:03 -0400

    Add boss file uploading and force-location option to analysis update.

--- src/main/scala/org/broadinstitute/dsde/vault/services/analysis/UpdateService.scala
+++ src/main/scala/org/broadinstitute/dsde/vault/services/analysis/UpdateService.scala
@@ -4,7 +4,7 @@ import javax.ws.rs.Path
 
 import akka.actor.Props
 import com.wordnik.swagger.annotations._
-import org.broadinstitute.dsde.vault.DmClientService
+import org.broadinstitute.dsde.vault.{BossClientService, DmClientService}
 import org.broadinstitute.dsde.vault.model.AnalysisJsonProtocol.impAnalysisUpdate
 import org.broadinstitute.dsde.vault.model._
 import spray.http.MediaTypes._
@@ -23,7 +23,9 @@ trait UpdateService extends HttpService {
     httpMethod = "POST",
     produces = "application/json",
     consumes = "application/json",
-    notes = "Accepts a json packet as POST. Updates the Vault analysis object with the supplied output files.")
+    notes = "Accepts a json packet as POST. Updates the Vault analysis object with the supplied output files. " +
+      "Returns the Vault ID of the created object. If a custom header of type 'X-Force-Location' has a " +
+      "case-insensitive value of 'true', then the file path locations will be used as the location of the file object.")
   @ApiImplicitParams(Array(
     new ApiImplicitParam(name = "id", required = true, dataType = "string", paramType = "path", value = "Analysis Vault ID"),
     new ApiImplicitParam(name = "body", required = true, dataType = "org.broadinstitute.dsde.vault.model.AnalysisUpdate", paramType = "body", value = "Analysis outputs to add")
@@ -38,14 +40,18 @@ trait UpdateService extends HttpService {
     path("analyses" / Segment / "outputs") {
       id => {
         post {
-          respondWithMediaType(`application/json`) {
-            entity(as[AnalysisUpdate]) {
-              update =>
-                requestContext => {
-                  val dmService = actorRefFactory.actorOf(Props(new DmClientService(requestContext)))
-                  val updateActor = actorRefFactory.actorOf(UpdateServiceHandler.props(requestContext, dmService))
-                  updateActor ! UpdateServiceHandler.UpdateMessage(id, update)
-                }
+          optionalHeaderValueByName("X-Force-Location") {
+            forceLocation =>
+            respondWithMediaType(`application/json`) {
+              entity(as[AnalysisUpdate]) {
+                update =>
+                  requestContext => {
+                    val bossService = actorRefFactory.actorOf(BossClientService.props(requestContext))
+                    val dmService = actorRefFactory.actorOf(Props(new DmClientService(requestContext)))
+                    val updateActor = actorRefFactory.actorOf(UpdateServiceHandler.props(requestContext, dmService, bossService))
+                    updateActor ! UpdateServiceHandler.UpdateMessage(id, update, forceLocation)
+                  }
+              }
             }
           }
         }
