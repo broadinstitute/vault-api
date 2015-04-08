f07201aa5756b83187e8c7ddbe04ffbdaa08d95b
Khalid Shakir <kshakir@broadinstitute.org>
2015-04-08 18:29:01 -0300

    Common code removed, replaced with vault-common dependency. DSDEV-1820

--- src/main/scala/org/broadinstitute/dsde/vault/common/openam/OpenAMSession.scala
+++
@@ -1,6 +0,0 @@
-package org.broadinstitute.dsde.vault.common.openam
-
-import spray.http.HttpCookie
-import spray.http.HttpHeaders.Cookie
-
-object OpenAMSession extends Cookie(Seq(HttpCookie(OpenAMConfig.tokenCookie, OpenAMClient.authenticate().tokenId)))
